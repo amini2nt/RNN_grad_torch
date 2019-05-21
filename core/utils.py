4aa09e00b9b3364189cff5774952df644ddbec41
apsarath <sarathcse2008@gmail.com>
2019-05-20 23:03:15 -0400

    optimizer names added

--- core/utils.py
+++ core/utils.py
@@ -13,19 +13,33 @@ def get_optimizer(config):
 	elif config.optim_name == "torch_RMSprop":
 		return torch_RMSprop(learning_rate=config.lr, alpha=config.alpha, eps=config.eps)
 	elif config.optim_name == "WA_RMSprop":
-		return WA_RMSprop(learning_rate=config.lr, alpha=config.alpha, eps=config.eps)	
+		return WA_RMSprop(learning_rate=config.lr, alpha=config.alpha, eps=config.eps)
+	elif config.optim_name == "torch_WA_RMSprop":
+		return torch_WA_RMSprop(learning_rate=config.lr, alpha=config.alpha, eps=config.eps)	
 	elif config.optim_name == "ADAM":
 		return ADAM(learning_rate=config.lr, beta1 = config.beta1, beta2 = config.beta2, eps=config.eps)
+	elif config.optim_name == "torch_ADAM":
+		return torch_ADAM(learning_rate=config.lr, beta1 = config.beta1, beta2 = config.beta2, eps=config.eps)
 	elif config.optim_name == "WA_ADAM":
 		return WA_ADAM(learning_rate=config.lr, beta1 = config.beta1, beta2 = config.beta2, eps=config.eps)
+	elif config.optim_name == "torch_WA_ADAM":
+		return torch_WA_ADAM(learning_rate=config.lr, beta1 = config.beta1, beta2 = config.beta2, eps=config.eps)
 	elif config.optim_name == "ADADELTA":
 		return ADADELTA(learning_rate=config.lr, gamma=config.gamma, eps=config.eps)
+	elif config.optim_name == "torch_ADADELTA":
+		return torch_ADADELTA(learning_rate=config.lr, gamma=config.gamma, eps=config.eps)
 	elif config.optim_name == "WA_ADADELTA":
-		return WA_ADADELTA(learning_rate=config.lr, gamma=config.gamma, eps=config.eps)		
+		return WA_ADADELTA(learning_rate=config.lr, gamma=config.gamma, eps=config.eps)	
+	elif config.optim_name == "torch_WA_ADADELTA":
+		return torch_WA_ADADELTA(learning_rate=config.lr, gamma=config.gamma, eps=config.eps)			
 	elif config.optim_name == "ADAGRAD":
 		return ADAGRAD(learning_rate=config.lr, eps=config.eps)
+	elif config.optim_name == "torch_ADAGRAD":
+		return torch_ADAGRAD(learning_rate=config.lr, eps=config.eps)
 	elif config.optim_name == "WA_ADAGRAD":
 		return WA_ADAGRAD(learning_rate=config.lr, eps=config.eps)
+	elif config.optim_name == "torch_WA_ADAGRAD":
+		return torch_WA_ADAGRAD(learning_rate=config.lr, eps=config.eps)
 	elif config.optim_name == "TA_ADAM":
 		return TA_ADAM(learning_rate=config.lr, beta1 = config.beta1, beta2 = config.beta2, eps=config.eps, max_steps=config.max_steps)	
 	elif config.optim_name == "TA_RMSprop":
