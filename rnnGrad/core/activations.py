ec204881e8c80f1ec13b373f4b7126463ca87578
apsarath <sarathcse2008@gmail.com>
2019-05-23 22:13:54 -0400

    test for losses

--- rnnGrad/core/activations.py
+++ rnnGrad/core/activations.py
@@ -1,48 +1,5 @@
-import numpy as np
-from scipy.special import expit
-from rnnGrad.core.model import Layer
 import torch
-
-class torch_sigmoid(Layer):
-	"""Implementation of sigmoid activation function."""
-
-	def __init__(self):
-		"""Initializes a sigmoid layer."""
-		super().__init__()
-		self._output = {}
-
-	def forward(self, input, time=1):
-		"""Forward prop through the sigmoid layer.
-		
-		Args:
-			input: numpy array, input to the sigmoid layer
-			time: int, time step
-
-		returns:
-			output, a numpy array of same size as input.
-		"""
-		self._output[time] = torch.sigmoid(input)
-		return self._output[time]
-
-	def backward(self, in_grad, time=1):
-		"""backward prop through the sigmoid layer.
-
-		Args:
-			in_grad: numpy array, incoming gradient.
-			time: int, time step
-
-		returns:
-			numpy array, outgoing gradient.
-		"""
-		self._grads[time] = self._output[time] * (1 - self._output[time])
-		self._grads[time] = torch.t(self._grads[time]) * in_grad
-		return self._grads[time]
-
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._output = {}
-		self._grads = {}
-		self._updates = {}	
+from rnnGrad.core.model import Layer
 
 
 class sigmoid(Layer):
@@ -57,76 +14,34 @@ class sigmoid(Layer):
 		"""Forward prop through the sigmoid layer.
 		
 		Args:
-			input: numpy array, input to the sigmoid layer
+			input: torch array, input to the sigmoid layer
 			time: int, time step
 
 		returns:
-			output, a numpy array of same size as input.
+			output, a torch array of same size as input.
 		"""
-		self._output[time] = expit(input)
+		self._output[time] = torch.sigmoid(input)
 		return self._output[time]
 
 	def backward(self, in_grad, time=1):
 		"""backward prop through the sigmoid layer.
 
 		Args:
-			in_grad: numpy array, incoming gradient.
+			in_grad: torch array, incoming gradient.
 			time: int, time step
 
 		returns:
-			numpy array, outgoing gradient.
+			torch array, outgoing gradient.
 		"""
 		self._grads[time] = self._output[time] * (1 - self._output[time])
-		self._grads[time] = self._grads[time].T * in_grad
-		return self._grads[time]
-
+		return torch.t(self._grads[time]) * in_grad
+		
 	def reset_computation(self):
 		"""resets the layer computations."""
 		self._output = {}
 		self._grads = {}
 		self._updates = {}	
 
-
-class torch_tanh(Layer):
-	"""Implementation of tanh activation function."""
-
-	def __init__(self):
-		"""Initializes a tanh layer."""
-		super().__init__()
-		self._output = {}
-		
-	def forward(self, input, time=1):
-		"""Forward prop through the tanh layer.
-		
-		Args:
-			input: numpy array, input to the tanh layer
-			time: int, time step
-
-		returns:
-			output, a numpy array of same size as input.
-		"""
-		self._output[time] = torch.tanh(input)
-		return self._output[time]
-
-	def backward(self, in_grad, time=1):
-		"""backward prop through the tanh layer.
-
-		Args:
-			in_grad: numpy array, incoming gradient.
-			time: int, time step
-
-		returns:
-			numpy array, outgoing gradient.
-		"""
-		self._grads[time] = 1 - (self._output[time]**2)
-		self._grads[time] = torch.t(self._grads[time]) * in_grad
-		return self._grads[time]
-
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._output = {}
-		self._grads = {}
-		self._updates = {}
 
 class tanh(Layer):
 	"""Implementation of tanh activation function."""
@@ -140,71 +55,33 @@ class tanh(Layer):
 		"""Forward prop through the tanh layer.
 		
 		Args:
-			input: numpy array, input to the tanh layer
+			input: torch array, input to the tanh layer
 			time: int, time step
 
 		returns:
-			output, a numpy array of same size as input.
+			output, a torch array of same size as input.
 		"""
-		self._output[time] = np.tanh(input)
+		self._output[time] = torch.tanh(input)
 		return self._output[time]
 
 	def backward(self, in_grad, time=1):
 		"""backward prop through the tanh layer.
 
 		Args:
-			in_grad: numpy array, incoming gradient.
+			in_grad: torch array, incoming gradient.
 			time: int, time step
 
 		returns:
-			numpy array, outgoing gradient.
+			torch array, outgoing gradient.
 		"""
 		self._grads[time] = 1 - (self._output[time]**2)
-		self._grads[time] = self._grads[time].T * in_grad
-		return self._grads[time]
-
+		return torch.t(self._grads[time]) * in_grad
+		
 	def reset_computation(self):
 		"""resets the layer computations."""
 		self._output = {}
 		self._grads = {}
 		self._updates = {}
-
-class torch_relu(Layer):
-	"""Implementation of relu activation function."""
-
-	def __init__(self):
-		"""Initializes a relu layer."""
-		super().__init__()
-		self._output = {}
-		
-	def forward(self, input, time=1):
-		"""Forward prop through the relu layer.
-		
-		Args:
-			input: numpy array, input to the relu layer
-			time: int, time step
-
-		returns:
-			output, a numpy array of same size as input.
-		"""
-		self._output[time] = torch.max(torch.Tensor([0]), input)
-		return self._output[time]
-
-	def backward(self, in_grad, time=1):
-		"""backward prop through the relu layer.
-
-		Args:
-			in_grad: numpy array, incoming gradient.
-			time: int, time step
-
-		returns:
-			numpy array, outgoing gradient.
-		"""
-
-		self._grads[time] = torch.gt(self._output[time],0).float() ## not sure why the max above shouldnt be used or x[x > 0]
-		self._grads[time] = torch.t(self._grads[time]) * in_grad
-		return self._grads[time]
-
 
 
 class relu(Layer):
@@ -219,34 +96,28 @@ class relu(Layer):
 		"""Forward prop through the relu layer.
 		
 		Args:
-			input: numpy array, input to the relu layer
+			input: torch array, input to the relu layer
 			time: int, time step
 
 		returns:
-			output, a numpy array of same size as input.
+			output, a torch array of same size as input.
 		"""
-		self._output[time] = np.maximum(0, input)
+		self._output[time] = torch.max(torch.Tensor([0]), input)
 		return self._output[time]
 
 	def backward(self, in_grad, time=1):
 		"""backward prop through the relu layer.
 
 		Args:
-			in_grad: numpy array, incoming gradient.
+			in_grad: torch array, incoming gradient.
 			time: int, time step
 
 		returns:
-			numpy array, outgoing gradient.
+			torch array, outgoing gradient.
 		"""
-		self._grads[time] = np.greater(self._output[time], 0).astype(float)
-		self._grads[time] = self._grads[time].T * in_grad
-		return self._grads[time]
 
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._output = {}
-		self._grads = {}
-		self._updates = {}		
+		self._grads[time] = torch.gt(self._output[time],0).float()
+		return torch.t(self._grads[time]) * in_grad
 
 
 def get_activation(activation):
@@ -260,13 +131,9 @@ def get_activation(activation):
 	"""
 	if activation=="sigmoid":
 		return sigmoid()
-	elif activation=="torch_sigmoid":
-		return torch_sigmoid()
+
 	elif activation=="tanh":
 		return tanh()
-	elif activation=="torch_tanh":
-		return torch_tanh()
+
 	elif activation=="relu":
 		return relu()
-	elif activation=="torch_relu":
-		return torch_relu()
--- rnnGrad/core/losses.py
+++ rnnGrad/core/losses.py
@@ -2,68 +2,6 @@ import numpy as np
 from scipy.special import expit
 import torch
 
-class torch_l2_loss(object):
-	"""Implementation of L2 loss."""
-
-	def __init__(self, average='mean'):
-		"""Initializes the L2 loss.
-
-		Args:
-			average: str, 'none' for not averaging the loss over minibatch,
-					 'mean' for averaging, and 'sum' for summing.
-		"""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-		self._average = average
-		
-	def forward(self, pred, target, time=1):
-		"""forward prop through the loss.
-
-		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size, num_outputs), target prediction.
-			time: int, time step
-
-		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
-		"""
-		self._pred[time] = pred
-		self._target[time] = target
-		loss = ((pred - target)**2)/2
-		if self._average == 'none':
-			self._output[time] = torch.sum(loss, dim=1)
-		elif self._average == 'sum':
-			self._output[time] = torch.sum(loss)
-		elif self._average == 'mean':
-			self._output[time] = torch.mean(loss)
-		return self._output[time]
-
-	def backward(self, time=1):
-		"""backward prop through the loss.
-		
-		Args:
-			time: int, time step
-
-		returns:
-			gradient matrix of size (num_outputs, batch_size)
-		"""
-		self._grad[time] = torch.t((self._pred[time] - self._target[time]))
-		if self._average == 'mean':
-			self._grad[time] /= self._pred[time].numel()
-		return self._grad[time]
-
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-
-
-
-
 
 class l2_loss(object):
 	"""Implementation of L2 loss."""
@@ -85,22 +23,22 @@ class l2_loss(object):
 		"""forward prop through the loss.
 
 		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size, num_outputs), target prediction.
+			pred: torch array of size (batch size, num_outputs), model's prediction.
+			target: torch array of size (batch size, num_outputs), target prediction.
 			time: int, time step
 
 		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
+			loss which is a matrix if average is 'none', a scalar otherwise.
 		"""
 		self._pred[time] = pred
 		self._target[time] = target
-		loss = ((pred - target)**2)/2
+		loss = (pred - target)**2
 		if self._average == 'none':
-			self._output[time] = np.sum(loss, axis=1)
+			self._output[time] = loss
 		elif self._average == 'sum':
-			self._output[time] = np.sum(loss)
+			self._output[time] = torch.sum(loss)
 		elif self._average == 'mean':
-			self._output[time] = np.mean(loss)
+			self._output[time] = torch.mean(loss)
 		return self._output[time]
 
 	def backward(self, time=1):
@@ -112,9 +50,9 @@ class l2_loss(object):
 		returns:
 			gradient matrix of size (num_outputs, batch_size)
 		"""
-		self._grad[time] = (self._pred[time] - self._target[time]).T
+		self._grad[time] = torch.t((self._pred[time] - self._target[time]))*2
 		if self._average == 'mean':
-			self._grad[time] /= self._pred[time].size
+			self._grad[time] /= self._pred[time].numel()
 		return self._grad[time]
 
 	def reset_computation(self):
@@ -145,22 +83,22 @@ class bce_loss(object):
 		"""forward prop through the loss.
 
 		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size, num_outputs), target prediction.
+			pred: torch array of size (batch size, num_outputs), model's prediction.
+			target: torch array of size (batch size, num_outputs), target prediction.
 			time: int, time step
 
 		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
+			loss which is a matrix if average is 'none', a scalar otherwise.
 		"""
 		self._pred[time] = pred
 		self._target[time] = target
-		loss = -target*np.log(pred) - (1-target)*np.log(1-pred)
+		loss = -target * torch.log(pred) - (1-target) * torch.log(1-pred)
 		if self._average == 'none':
-			self._output[time] = np.sum(loss, axis=1)
+			self._output[time] = loss
 		elif self._average == 'sum':
-			self._output[time] = np.sum(loss)
+			self._output[time] = torch.sum(loss)
 		elif self._average == 'mean':
-			self._output[time] = np.mean(loss)
+			self._output[time] = torch.mean(loss)
 		return self._output[time]
 
 	def backward(self, time=1):
@@ -172,9 +110,11 @@ class bce_loss(object):
 		returns:
 			gradient matrix of size (num_outputs, batch_size)
 		"""
-		self._grad[time] = ((-self._target[time]/(self._pred[time])) + ((1-self._target[time])/(1-self._pred[time]))).T
+		self._grad[time] = (-self._target[time]/(self._pred[time])) 
+		self._grad[time] += (1-self._target[time])/(1-self._pred[time])
+		self._grad[time] = torch.t(self._grad[time])
 		if self._average == 'mean':
-			self._grad[time] /= self._pred[time].size
+			self._grad[time] /= self._pred[time].numel()
 		return self._grad[time]
 
 	def reset_computation(self):
@@ -185,11 +125,11 @@ class bce_loss(object):
 		self._grad = {}
 		
 
-class torch_bce_with_logits_loss(object):
-	"""Implementation of binary cross entropy loss."""
+class bce_with_logits_loss(object):
+	"""Implementation of binary cross entropy loss with logits."""
 
 	def __init__(self, average='mean'):
-		"""Initializes the BCE loss.
+		"""Initializes the BCE loss with logits.
 
 		Args:
 			average: str, 'none' for not averaging the loss over minibatch,
@@ -205,20 +145,19 @@ class torch_bce_with_logits_loss(object):
 		"""forward prop through the loss.
 
 		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size, num_outputs), target prediction.
+			pred: torch array of size (batch size, num_outputs), model's prediction.
+			target: torch array of size (batch size, num_outputs), target prediction.
 			time: int, time step
 
 		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
+			loss which is a matrix if average is 'none', a scalar otherwise.
 		"""
 		self._pred[time] = pred
 		self._target[time] = target
-
-
-		loss = target*(torch.log(torch.exp(torch.zeros(1))+torch.exp(-pred))) + (1-target)*(torch.log(torch.exp(torch.zeros(1))+torch.exp(pred)))
+		loss = target * (torch.log(torch.exp(torch.zeros(1)) + torch.exp(-pred))) 
+		loss += (1-target) * (torch.log(torch.exp(torch.zeros(1)) + torch.exp(pred)))
 		if self._average == 'none':
-			self._output[time] = torch.sum(loss, dim=1)
+			self._output[time] = loss
 		elif self._average == 'sum':
 			self._output[time] = torch.sum(loss)
 		elif self._average == 'mean':
@@ -246,66 +185,8 @@ class torch_bce_with_logits_loss(object):
 		self._output = {}
 		self._grad = {}
 
-class bce_with_logits_loss(object):
-	"""Implementation of binary cross entropy loss."""
 
-	def __init__(self, average='mean'):
-		"""Initializes the BCE loss.
-
-		Args:
-			average: str, 'none' for not averaging the loss over minibatch,
-					 'mean' for averaging, and 'sum' for summing.
-		"""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-		self._average = average
-		
-	def forward(self, pred, target, time=1):
-		"""forward prop through the loss.
-
-		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size, num_outputs), target prediction.
-			time: int, time step
-
-		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
-		"""
-		self._pred[time] = pred
-		self._target[time] = target
-		loss = target*(np.logaddexp(0, -pred)) + (1-target)*(np.logaddexp(0, pred))
-		if self._average == 'none':
-			self._output[time] = np.sum(loss, axis=1)
-		elif self._average == 'sum':
-			self._output[time] = np.sum(loss)
-		elif self._average == 'mean':
-			self._output[time] = np.mean(loss)
-		return self._output[time]
-
-	def backward(self, time=1):
-		"""backward prop through the loss.
-
-		Args:
-			time: int, time step
-
-		returns:
-			gradient matrix of size (num_outputs, batch_size)
-		"""
-		self._grad[time] = (expit(self._pred[time]) - self._target[time]).T
-		if self._average == 'mean':
-			self._grad[time] /= self._pred[time].size
-		return self._grad[time]
-
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-
-class torch_ce_with_logits_loss(object):
+class ce_with_logits_loss(object):
 	"""Implementation of cross entropy loss with logits."""
 
 	def __init__(self, average='mean'):
@@ -323,11 +204,11 @@ class torch_ce_with_logits_loss(object):
 	def forward(self, pred, target, time=1):
 		"""forward prop through the loss.
 		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size), target prediction ids.
+			pred: torch array of size (batch size, num_outputs), model's prediction.
+			target: torch array of size (batch size), target prediction ids.
 			time: int, time step
 		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
+			loss which is a matrix if average is 'none', a scalar otherwise.
 		"""
 		self._pred[time] = pred
 		self._target[time] = target
@@ -362,64 +243,3 @@ class torch_ce_with_logits_loss(object):
 		self._target = {}
 		self._output = {}
 		self._grad = {}
-
-class ce_with_logits_loss(object):
-	"""Implementation of cross entropy loss with logits."""
-
-	def __init__(self, average='mean'):
-		"""Initializes the CE loss with logits.
-		Args:
-			average: str, 'none' for not averaging the loss over minibatch,
-					 'mean' for averaging, and 'sum' for summing.
-		"""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-		self._average = average
-		
-	def forward(self, pred, target, time=1):
-		"""forward prop through the loss.
-		Args:
-			pred: numpy array of size (batch size, num_outputs), model's prediction.
-			target: numpy array of size (batch size), target prediction ids.
-			time: int, time step
-		returns:
-			loss which is a vector if average is 'none', a scalar otherwise.
-		"""
-		self._pred[time] = pred
-		self._target[time] = target
-		logits_for_answers = pred[np.arange(len(pred)), target]
-		loss = - logits_for_answers + np.log(np.sum(np.exp(pred), axis=-1))
-		if self._average == 'none':
-			self._output[time] = loss
-		elif self._average == 'sum':
-			self._output[time] = np.sum(loss)
-		elif self._average == 'mean':
-			self._output[time] = np.mean(loss)
-		return self._output[time]
-
-	def backward(self, time=1):
-		"""backward prop through the loss.
-		Args:
-			time: int, time step
-		returns:
-			gradient matrix of size (num_outputs, batch_size)
-		"""
-		ones_for_answers = np.zeros_like(self._pred[time])
-		ones_for_answers[np.arange(len(self._pred[time])), self._target[time]] = 1
-		softmax = np.exp(self._pred[time])/ np.exp(self._pred[time]).sum(axis=1, keepdims=True)
-		self._grad[time] = (-ones_for_answers + softmax)
-		if self._average == 'mean':
-			self._grad[time] /= self._pred[time].shape[0]
-		return self._grad[time]
-
-	def reset_computation(self):
-		"""resets the layer computations."""
-		self._pred = {}
-		self._target = {}
-		self._output = {}
-		self._grad = {}
-
-
-
